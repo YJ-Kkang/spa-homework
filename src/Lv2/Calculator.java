7ca5777a7079553241307281f9d44c7d0bca7941
YJ <kyujin995@gmail.com>
2024-11-20 13:24:07 +0900

    git commit -m "chore: 주석 및 설명 추가"

--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
+    //연산 결과 저장할 리스트 선언
     private List<Double> results = new ArrayList<>();
 
+    //두 정수와 연산자를 받아 계산을 수행하는 메서드
     public double calculate(int num1, int num2, char operator) {
         double result = 0;
 
+        //연산 수행
         if (operator == '+') {
             result = num1 + num2;
         } else if (operator == '-') {
@@ -17,18 +20,25 @@ public class Calculator {
         } else if (operator == '/') {
             if (num2 == 0) {
                 System.out.println("0은 분모가 될 수 없습니다.");
+                //0으로 나누기 시도 시 예외 처리
+                /* NaN은 수학적으로 정의되지 않은 연산의 결과를 나타내기 위해 사용하는 특수한 값
+                주로 실수 연산에서 사용되기에 double 또는 float 타입에 속함 */
                 return Double.NaN;
             }
+            //입력을 다 int로 받았기에 result 값 출력할 때는 형 변환
             result = (double) num1 / num2;
         } else {
             System.out.println("유효하지 않은 연산자입니다.");
+            //잘못된 연산자 입력 시 예외 처리
             return Double.NaN;
         }
 
+        //연산 결과 리스트에 추가
         results.add(result);
         return result;
     }
 
+    //결과 리스트 반환
     public List<Double> getResults() {
         return new ArrayList<>(results);
     }
--- src/Lv2/Input.java
+++ src/Lv2/Input.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Input {
     private Scanner scanner;
 
+    //Input 클래스의 생성자. Scanner 객체를 초기화
     public Input() {
         this.scanner = new Scanner(System.in);
     }
@@ -13,20 +14,26 @@ public class Input {
         System.out.print(prompt);
         int number = scanner.nextInt();
         if (number < 0) {
+            // 음의 정수 입력 시 프로그램 종료
+            /*System.exit(1);는 자바 프로그램에서 현재 실행 중인 Java Virtual Machine(JVM)을 종료하는 명령.
+            이 코드는 프로그램을 즉시 종료하고, 종료 상태 코드 반환(0이면 정상 종료, 1 또는 다른 양수 값이면 오류 또는 비정상 종료) */
             System.out.println("음의 정수는 입력할 수 없습니다. 계산기를 종료합니다.");
-            System.exit(1); // 프로그램 종료
+            System.exit(1);
         }
-        return number; //입력된 숫자 반환
+        //입력된 숫자 반환
+        return number;
     }
 
-    //연산자 입력
+    //연산자 입력(연산자를 입력받는 메서드 getOperator)
     public char getOperator(String prompt) {
         System.out.print(prompt);
         char operator = scanner.next().charAt(0);
         if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+            // 잘못된 연산자 입력 시 프로그램 종료
             System.out.println("유효하지 않은 연산자입니다.");
-            System.exit(1); // 프로그램 종료
+            System.exit(1);
         }
-        return operator; //입력된 연산자 반환
+        //입력된 연산자 반환
+        return operator;
     }
 }
